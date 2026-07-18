1d03585e0721abd790d509e3359cbe88b7666cd5
Kevin Antonino <kevin.antonino@gmail.com>
2026-07-18 08:44:48 -0700

    postal service update

--- messaging.py
+++ messaging.py
@@ -1,5 +1,6 @@
 from collections import deque
 from dataclasses import dataclass
+from queue import PriorityQueue
 from enum import Enum, auto
 
 class Actions(Enum):
@@ -11,69 +12,98 @@ class Actions(Enum):
 
 @dataclass(frozen=True)
 class Message:
-    self.sender: "Process"
-    self.receiver: "Process"
-    self.action: Action 
-    self.timestamp: float
+    sender: "Process"
+    receiver: "Process"
+    action: Actions
+    timestamp: float
 
-    def __post_init__:
-        self.timestamp = self.sender.timestamp
+    def __post_init__(self):
+        object.__setattr__(self, 'timestamp', self.sender.timestamp) # yucky
 
 @dataclass(frozen=True)
 class Start(Message):
-    self.action: Actions.START
+    action: Actions = Actions.START
 
 @dataclass(frozen=True)
 class Terminate(Message):
-    self.action: Actions.TERMINATE
+    action: Actions = Actions.TERMINATE
 
 @dataclass(frozen=True)
 class InitializationComplete(Message):
-    self.action: Actions.INIT_COMPLETE
+    action: Actions = Actions.INIT_COMPLETE
 
 @dataclass(frozen=True)
 class SimulationComplete(Message):
-    self.action: Actions.SIM_COMPLETE
+    action: Actions = Actions.SIM_COMPLETE
 
 @dataclass(frozen=True)
 class OutputMessage(Message):
-    self.action: Actions.PULL
-    self.output = sender.output
+    action: Actions = Actions.PULL
+    output = sender.output
 
 class Mailbox():
     def __init__(self):
-        self.inbox: dict["Process", deque[Message]] = {} 
-        self.outbox: dict["Process", deque[Message]] = {} 
+        self.links: dict["Process", deque] = {}
+        self.inbox = PriorityQueue()
+        self.outbox = deque()
 
     def receive_message(self, msg):
-        if msg.sender not in self.inbox.keys():
+        if msg.sender not in self.links:
             raise ValueError(f'{msg.sender} not in inbox!') 
-        self.inbox[msg.sender].append(msg)
+
+        if not self.links[msg.sender]:
+            self.inbox.put((msg.timestamp, msg))
+
+        self.links[msg.sender].append(msg)
+
+    def add_to_outbox(self, msg):
+        self.outbox.append(msg)
 
     def get_next_message(self):
         next_msg = None
-        next_timestamp = float('inf')
-        for p in self.inbox.keys():
-            if not self.inbox[p]:
-                break
-            if self.inbox[p][0].timestamp < next_timestamp:
-                next_msg = self.inbox[p][0]
-                next_timestamp = next_msg.timestamp
-        if next_msg is not None:
-            self.inbox[next_msg.sender].popleft()
+        if self.inbox.qsize() == len(self.links.keys()):
+            next_msg = self.inbox.get()
+            self.links[next_msg.sender].popleft()
+            # If there is another message in the queue, put it into the inbox
+            queue = self.links[next_msg.sender]
+            if queue:
+                self.inbox.put((queue[0].timestamp, queue[0]))
 
         return next_msg
 
     def remove_process(self, p):
-        self.inbox.pop(self.sender)
-        self.outbox.pop(self.sender, 0)
+        self.links.pop(p)
    
     def add_sender(self, sender):
-        self.inbox[sender] = deque()
+        self.links[sender] = deque()
 
-    def add_receiver(self, receiver):
-        self.outbox[receiver] = deque()
+    def get_outbox(self):
+        return self.outbox
+
+class ControllerMailbox(Mailbox):
+    def __init__(self):
+        super().__init__(self):
+
+    def receive_message(self, msg):
+        self.inbox.put((msg.timestamp, msg))
+
+    def get_next_message(self):
+        next_msg = None
+        if self.inbox:
+            next_msg = self.inbox.get()
+
+        return next_msg
 
 class PostalService():
-    
+    def __init__(self):
+        self.mailboxes: dict["Process", Mailbox] = {}
+
+    def register(self, process):
+        self.mailboxes[process] = process.mailbox
+
+    def deliver(self, process):
+        msg_queue = self.mailboxes[process].get_outbox()
+        while msg_queue:
+            msg = msg_queue.pop()
+            self.mailboxes[msg.receiver].receive_message(msg)
 
