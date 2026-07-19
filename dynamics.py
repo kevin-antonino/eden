179c6de7e9a0f0f2d7cddf8af2c6c4eedbe67964
Kevin Antonino <kevin.antonino@gmail.com>
2026-07-18 20:33:18 -0700

    dynamic sys init commit

---
+++ dynamics.py
@@ -0,0 +1,35 @@
+from processes import PhysicalProcess:
+from abc import abstractmethod
+
+class DynamicSystem(Process):
+    def __init__(self, n_states: int, n_inputs: int, n_outputs: int, name: str ='n/a'):
+        super().__init__()
+        self.integrator = RungeKutta4() 
+        self.state  = zeros((n_states, 1))
+        self.input = zeros((n_inputs, 1))
+        self.output = zeros((n_inputs, 1))
+        self.x0 = zeros((n_states,1))
+
+    def evolve(self) -> None:
+        # Evaluate x[k] = F(x[k-1], u[k-1], k-1) 
+        self.state = self.integrator.integrate_system_dynamics(
+            self.state, self.input, self.dynamic_equation, 
+            self.get_timestamp(), self.get_next_timestamp().now)
+        
+        # Evaluate y[k] = G(x[k], u[k], k)
+        self.output  = self.output_equation(
+            self.state, self.input, self.get_timestamp())
+
+    @abstractmethod
+    def dynamic_equation(self, x: ndarray, u: ndarray, t: float) -> ndarray:
+        '''
+        xdot = f(x, u, t)
+        '''
+        ...
+    
+    @abstractmethod
+    def output_equation(self, x: ndarray, u: ndarray, t: float) -> ndarray:
+        '''
+        y = g(x, u, t)
+        '''
+        ...
