765e270a12adac4d9e33ae2d8ffe22261c8d4868
Kevin Antonino <kevin.antonino@gmail.com>
2026-07-24 17:39:16 -0700

    linear systems work now

--- dynamics.py
+++ dynamics.py
@@ -1,8 +1,10 @@
-from processes import PhysicalProcess:
+from processes import PhysicalProcess
+from integrators import RungeKutta4
 from abc import abstractmethod
+from numpy import zeros
 
 class DynamicSystem(PhysicalProcess):
-    def __init__(self, n_states: int, n_inputs: int, n_outputs: int, name: str ='n/a'):
+    def __init__(self, n_states: int, n_inputs: int, n_outputs: int):
         super().__init__()
         self.integrator = RungeKutta4() 
         self.state  = zeros((n_states, 1))
@@ -13,7 +15,7 @@ class DynamicSystem(PhysicalProcess):
         # Evaluate x[k] = F(x[k-1], u[k-1], k-1) 
         self.state = self.integrator.integrate_system_dynamics(
             self.state, self.input, self.dynamic_equation, 
-            self.get_timestamp(), self.get_next_timestamp().now)
+            self.get_timestamp(), self.get_next_timestamp())
         
         # Evaluate y[k] = G(x[k], u[k], k)
         self.output  = self.output_equation(
@@ -34,9 +36,9 @@ class DynamicSystem(PhysicalProcess):
         ...
 
 class LinearSystem(DynamicSystem):
-    def __init__(self, A: ndarray, B: ndarray, C: ndarray, D: ndarray, name: str ='n/a'):
+    def __init__(self, A: ndarray, B: ndarray, C: ndarray, D: ndarray):
         self.A, self.B, self.C, self.D = A, B, C, D # System Matrices
-        super().__init__(self, A.shape[1], B.shape[1], C.shape[0], name=name)
+        super().__init__(A.shape[1], B.shape[1], C.shape[0])
 
     def dynamic_equation(self, x: ndarray, u: ndarray, t: float) -> ndarray:
         xdot = self.A @ x + self.B @ u
@@ -44,4 +46,5 @@ class LinearSystem(DynamicSystem):
 
     def output_equation(self, x: ndarray, u: ndarray, t: float) -> ndarray:
         y = self.C @ x + self.D @ u
+        print(y)
         return y
--- integrators.py
+++ integrators.py
@@ -1,4 +1,4 @@
-rom abc import ABC, abstractmethod
+from abc import ABC, abstractmethod
 from typing import Callable
 from numpy import ndarray
 
