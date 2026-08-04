8112051da139262465149e38237d2f2b05743189
Kevin Antonino <kevin.antonino@gmail.com>
2026-08-03 21:12:06 -0700

    removing print

--- dynamics.py
+++ dynamics.py
@@ -44,7 +44,5 @@ class LinearSystem(DynamicSystem):
         xdot = self.A @ x + self.B @ u
         return xdot
 
-    def output_equation(self, x: ndarray, u: ndarray, t: float) -> ndarray:
-        y = self.C @ x + self.D @ u
-        print(y)
+    def output_equation(self, x: ndarray, u: ndarray, t: float) -> ndarray: y = self.C @ x + self.D @ u
         return y
