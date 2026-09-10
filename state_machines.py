68452fb68d56bd154060cdba927ccf14d89a3abb
Kevin Antonino <kevin.antonino@gmail.com>
2026-09-09 18:01:30 -0700

    state machines initial commit

---
+++ state_machines.py
@@ -0,0 +1,85 @@
+from collections.abc import Callable
+from abc import ABC, abstractmethod
+from enum import Enum
+
+class SimulationStates(Enum):
+    ENGAGED     = auto()
+    DISENGAGED  = auto()
+
+class ModelStates(Enum):
+    REQUESTING  = auto()
+    WAITING     = auto()
+    EVOLVING    = auto()
+
+class StateMachine(ABC):
+    def __init__(self):
+        self.state = None
+        self.transition_map = {}
+
+    def add_state(self, name: Enum, transition: Callable[[str], Enum | None]):
+        self.transition_map[name] = transition
+   
+    def set_init_state(self, desired_state: str):
+        if desired_state not in self.transition_map.keys():
+            raise ValueError('Init state not in list of states!')
+        self.state = desired_state
+
+    def trigger(self, trig_txt: str):
+        transition = self.transition_map[self.state]
+        next_state = transition(trig_txt)    
+        if next_state not in self.transition_map.keys():
+            raise ValueError(f'{state} isn\'t a state in this machine')
+        self.state = next_state
+
+    def get_state(self):
+        return self.state
+
+class ModelStateMachine(StateMachine):
+    def __init__(self):
+        super().__init__()
+        self.add_state(ModelStates.REQUESTING, self.requesting_transition)
+        self.add_state(ModelStates.WAITING, self.waiting_transition)
+        self.add_state(ModelStates.EVOLVING, self.evolving_transition)
+        self.set_init_state(ModelStates.WAITING)
+
+    def requesting_transition(self, trig_txt):
+        if trig_txt == 'DATA_PUSHED':
+            return ModelStates.WAITING
+        else:
+            return None
+
+    def waiting_transition(self, trig_txt):
+        if trig_txt == 'NEED_DATA':
+            return ModelStates.REQUESTING
+
+        elif trig_txt == 'INCREMENT_TIME':
+            return ModelStates.EVOLVING
+        else:
+            return None
+
+    def evolving_transition(self, trig_txt):
+        if trig_txt == 'EVOLVE_DONE':
+            return ModelStates.REQUESTING
+        else:
+            return None
+
+class SimulationStateMachine(StateMachine):
+    def __init__(self):
+        super().__init__()
+        self.add_state(ModelStates.ENGAGED, self.engaged_transition)
+        self.add_state(ModelStates.DISENGAGED, self.disengaged_transition)
+        self.set_init_state(ModelStates.DISENGAGED)
+
+    def engaged_transition(self, trig_txt):
+        if trig_txt == 'LEAVING_TREE':
+            return SimulationStates.DISENGAGED
+        else:
+            return None
+
+    def disengaged_transition(self, trig_txt):
+        if trig_txt == 'JOINING_TREE'
+            return SimulationStates.ENGAGED
+        else:
+            return None
+
+
