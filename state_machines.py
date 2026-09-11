c69e4295e2dffb0bca722e68d569fa2ee4a11b0d
Kevin Antonino <kevin.antonino@gmail.com>
2026-09-11 14:24:52 -0700

    states are more clear now

--- state_machines.py
+++ state_machines.py
@@ -8,7 +8,7 @@ class SimulationStates(Enum):
 
 class ModelStates(Enum):
     REQUESTING  = auto() 
-    WAITING     = auto() 
+    PROCESSING  = auto() 
     EVOLVING    = auto() 
 
 class StateMachine(ABC):
@@ -38,28 +38,29 @@ class ModelStateMachine(StateMachine):
     def __init__(self):
         super().__init__()
         self.add_state(ModelStates.REQUESTING, self.requesting_transition)
-        self.add_state(ModelStates.WAITING, self.waiting_transition)
+        self.add_state(ModelStates.PROCESSING, self.processing_transition)
         self.add_state(ModelStates.EVOLVING, self.evolving_transition)
-        self.set_init_state(ModelStates.WAITING)
+        self.set_init_state(ModelStates.REQUESTING)
 
     def requesting_transition(self, trig_txt):
-        if trig_txt == 'DATA_READY':
-            return ModelStates.WAITING
+        if trig_txt == 'NEED_INPUTS':
+            return ModelStates.REQUESTING
+        elif trig_txt == 'INPUTS_READY':
+            return ModelStates.PROCESSING
         else:
             return None
 
-    def waiting_transition(self, trig_txt):
-        if trig_txt == 'NEED_DATA':
-            return ModelStates.REQUESTING
-
-        elif trig_txt == 'INCREMENT_TIME':
+    def processing_transition(self, trig_txt):
+        if trig_txt == 'INCREMENT_TIME':
             return ModelStates.EVOLVING
         else:
             return None
 
     def evolving_transition(self, trig_txt):
-        if trig_txt == 'EVOLVE_DONE':
+        if trig_txt == 'NEED_INPUTS':
             return ModelStates.REQUESTING
+        elif trig_txt == 'INPUTS_READY':
+            return ModelStates.PROCESSING
         else:
             return None
 
@@ -83,43 +84,3 @@ class SimulationStateMachine(StateMachine):
             return None
 
     
-## EXAMPLE CODE FOR MODEL OBJECT
-statemachine = ModelStateMachine()
-input_valid = {}
-
-def get_state(self):
-    return self.statemachine.get_state()
-
-def process_available_events(self):
-    if self.scheduler.get_next_event_time() < self.get_next_timestamp(): 
-        event = self.scheduler.pop_next_event()
-        self.process_event(event)
-
-def inputs_ready(self):
-    if self.input_valid is None or all(self.input_valid.values()):
-        return True
-    else:
-        return False
-
-def execute(self):
-    match self.get_state():
-        case ModelStates.REQUESTING: # Model is blocked & waiting for an input
-            self.process_available_events()
-            if self.inputs_ready():
-                self.statemachine.trigger('DATA_READY')
-        
-        case ModelStates.WAITING: # Input data is valid, model is waiting and processing events within [t, t+dt)
-            self.process_available_events()
-            if not self.inputs_ready(): 
-                self.statemachine.trigger('NEED_DATA')
-            
-            if self.scheduler.get_next_event_time() >= self.get_next_timestamp():
-                self.statemachine.trigger('INCREMENT_TIME')
-
-        case ModelStates.EVOLVING: # Model progressing time
-            self.evolve()
-            self.increment_time()
-            if self.logger:
-                self.log()
-            self.statemachine.trigger('EVOLVE_DONE')
-
